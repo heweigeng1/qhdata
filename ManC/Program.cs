63611b52bed6f471365c4cf0925b18b3a937d566
何伟庚 <heweigeng@hotmail.com>
2017-05-24 18:34:19 +0800

    发送get 与抓包

---
+++ ManC/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string data=  Get("http://hq.sinajs.cn/list=M0");
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+        public static string Get(string url)
+        {
+            string result = string.Empty;
+            try
+            {
+                HttpWebRequest wbRequest = (HttpWebRequest)WebRequest.Create(url);
+                wbRequest.Method = "GET";
+                wbRequest.ContentType = "text/html;charset=UTF-8";
+                HttpWebResponse wbResponse = (HttpWebResponse)wbRequest.GetResponse();
+                using (Stream responseStream = wbResponse.GetResponseStream())
+                {
+                    using (StreamReader sReader = new StreamReader(responseStream, Encoding.GetEncoding("gb2312")))
+                    {
+                        result = sReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return result;
+        }
+    }
+
+}
---
+++ sharppcap/Program.cs
@@ -0,0 +1,18 @@
+﻿using ServerToolCapture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharppcap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cap = WinCapHelper.WinCapInstance;
+            cap.Listen();
+        }
+    }
+}
