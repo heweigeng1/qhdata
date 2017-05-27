948681c6341383969d00cd466b0dc9e8dc0d976f
何伟庚 <heweigeng@hotmail.com>
2017-05-27 19:30:27 +0800

    上传

--- ManC/Program.cs
+++ ManC/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ManC
@@ -13,10 +15,30 @@ namespace ManC
     {
         static void Main(string[] args)
         {
-          string data=  Get("http://hq.sinajs.cn/list=RB1710");
-          var array=  data.Split('"');
-            QHModel model = new QHModel(array[1]);
-            Console.ReadKey();
+            while (true)
+            {
+                Show();
+            }
+
+        }
+        public static void Show()
+        {
+            string data = Get("http://hq.sinajs.cn/list=RB1710,RB1801,I1709,I1801");
+            //var data= Get("http://api.money.126.net/data/feed/FU_i1709");
+            List<List<string>> list = new List<List<string>>();
+            var str_body = data.Substring(0,data.Length-3).Split(';');
+            Regex reg = new Regex(@"[""].*?[""]");
+            var mathch = reg.Matches(data);
+            var head = $"期货名称\t编号\t开盘价\t最高价\t最低价\t收盘价\t买价\t卖价\t最新价\t结算价\t昨结算\t买量\t卖量\t持仓量\t成交量\t成交所\t简称\t日期";
+            Console.WriteLine(head);
+            foreach (Match m in mathch)
+            {
+                string str = m.Value.Substring(1, m.Value.Length - 2);
+                
+                QHModel model = new QHModel(str);
+            }
+            Thread.Sleep(1000);
+            Console.Clear();
         }
         public static string Get(string url)
         {
--- core/QHModel.cs
+++ core/QHModel.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace core
 {
@@ -16,42 +12,48 @@ namespace core
             if (!string.IsNullOrEmpty(data))
             {
                 var str = data.Split(',');
-                期货名称 = str[0];
-                Console.WriteLine($"期货名称:{期货名称}");
-                编号 = str[1];
-                Console.WriteLine($"编号:{编号}");
-                开盘价 = str[2].ToDec();
-                Console.WriteLine($"开盘价:{开盘价}");
-                最高价 = str[3].ToDec();
-                Console.WriteLine($"最高价:{最高价}");
-                最低价 = str[4].ToDec();
-                Console.WriteLine($"最低价:{最低价}");
-                昨日收盘价 = str[5].ToDec();
-                Console.WriteLine($"昨日收盘价:{昨日收盘价}");
-                买价 = str[6].ToDec();
-                Console.WriteLine($"买价:{买价}");
-                卖价 = str[7].ToDec();
-                Console.WriteLine($"卖价:{卖价}");
-                最新价 = str[8].ToDec();
-                Console.WriteLine($"最新价:{最新价}");
-                结算价 = str[9].ToDec();
-                Console.WriteLine($"结算价:{结算价}");
-                昨日结算 = str[10].ToDec();
-                Console.WriteLine($"昨日结算:{昨日结算}");
-                买量 = str[11].ToDec();
-                Console.WriteLine($"买量:{买量}");
-                卖量 = str[12].ToDec();
-                Console.WriteLine($"卖量:{卖量}");
-                持仓量 = str[13].ToDec();
-                Console.WriteLine($"持仓量:{持仓量}");
-                成交量 = str[14].ToDec();
-                Console.WriteLine($"成交量:{成交量}");
-                成交所 = str[15];
-                Console.WriteLine($"成交所:{成交所}");
-                品种简称 = str[16];
-                Console.WriteLine($"品种简称:{品种简称}");
-                日期 = str[17];
-                Console.WriteLine($"日期:{日期}");
+                for (int i = 0; i <= 17; i++)
+                {
+                    Console.Write(str[i]);
+                    Console.Write("\t");
+                }
+                Console.Write("\n");
+                //期货名称 = str[0];
+                //Console.WriteLine($"期货名称:{期货名称}");
+                //编号 = str[1];
+                //Console.WriteLine($"编号:{编号}");
+                //开盘价 = str[2].ToDec();
+                //Console.WriteLine($"开盘价:{开盘价}");
+                //最高价 = str[3].ToDec();
+                //Console.WriteLine($"最高价:{最高价}");
+                //最低价 = str[4].ToDec();
+                //Console.WriteLine($"最低价:{最低价}");
+                //昨日收盘价 = str[5].ToDec();
+                //Console.WriteLine($"昨日收盘价:{昨日收盘价}");
+                //买价 = str[6].ToDec();
+                //Console.WriteLine($"买价:{买价}");
+                //卖价 = str[7].ToDec();
+                //Console.WriteLine($"卖价:{卖价}");
+                //最新价 = str[8].ToDec();
+                //Console.WriteLine($"最新价:{最新价}");
+                //结算价 = str[9].ToDec();
+                //Console.WriteLine($"结算价:{结算价}");
+                //昨日结算 = str[10].ToDec();
+                //Console.WriteLine($"昨日结算:{昨日结算}");
+                //买量 = str[11].ToDec();
+                //Console.WriteLine($"买量:{买量}");
+                //卖量 = str[12].ToDec();
+                //Console.WriteLine($"卖量:{卖量}");
+                //持仓量 = str[13].ToDec();
+                //Console.WriteLine($"持仓量:{持仓量}");
+                //成交量 = str[14].ToDec();
+                //Console.WriteLine($"成交量:{成交量}");
+                //成交所 = str[15];
+                //Console.WriteLine($"成交所:{成交所}");
+                //品种简称 = str[16];
+                //Console.WriteLine($"品种简称:{品种简称}");
+                //日期 = str[17];
+                //Console.WriteLine($"日期:{日期}");
             }
         }
         public string 期货名称 { get; set; }
